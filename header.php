16c43d7720fd1b3a63ec3e5cfc213448be5fb668
Matthias Kettl <matthias.kettl@sosy.ifi.lmu.de>
2024-06-16 16:32:05 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Local Tennis Club</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<header>
+    <div class="container">
+        <h1>Local Tennis Club</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="results.php">Results</a></li>
+                <li><a href="teams.php">Teams</a></li>
+                <li><a href="board-members.php">Board Members</a></li>
+                <li><a href="privacy-policy.php">Privacy Policy</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
