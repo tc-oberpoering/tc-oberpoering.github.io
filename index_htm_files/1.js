dac10861f87d9ab6f89b9949463a9f798f96a5bb
Gerhard <diekettls@t-online.de>
2025-06-21 09:50:54 +0200

    Update website

--- index_htm_files/1.js
+++ index_htm_files/1.js
@@ -1,14 +1,14 @@
 window.local_table1_13_data = [
 {
  csv: "Mannschaft, interne Betreuer\u000A , \u000ATennisanf\u00E4nger, externes Training\u000AKleinfeld U9, Stefan Weinmann und Markus Steinhuber\u000ABambini U12, Michaela Zehrer und Daniela Huber\u000AM\u00E4dchen U15, Simone Gabler\u000ADamen-I, in Eigenregie\u000ADamen-II, in Eigenregie\u000AHerren-I, in Eigenregie\u000AHerren-II, in Eigenregie",
- first: "index_htm_files/19010.png",
- firstdark: "index_htm_files/19011.png",
- last: "index_htm_files/19012.png",
- lastdark: "index_htm_files/19013.png",
- next: "index_htm_files/19014.png",
- nextdark: "index_htm_files/19015.png",
- prev: "index_htm_files/19016.png",
- prevdark: "index_htm_files/19017.png",
+ first: "index_htm_files/18928.png",
+ firstdark: "index_htm_files/18929.png",
+ last: "index_htm_files/18930.png",
+ lastdark: "index_htm_files/18931.png",
+ next: "index_htm_files/18932.png",
+ nextdark: "index_htm_files/18933.png",
+ prev: "index_htm_files/18934.png",
+ prevdark: "index_htm_files/18935.png",
  rows: "12",
  theme: "1"
 }
--- index_htm_files/3.js
+++ index_htm_files/3.js
@@ -1,14 +1,14 @@
 window.local_table1_45_data = [
 {
  csv: "Damen:\u000A\u000A1. Sabine Bauriedl\u000A2. Emma F\u00FChrmann\u000A3. Helena Freundorfer",
- first: "index_htm_files/19010.png",
- firstdark: "index_htm_files/19011.png",
- last: "index_htm_files/19012.png",
- lastdark: "index_htm_files/19013.png",
- next: "index_htm_files/19014.png",
- nextdark: "index_htm_files/19015.png",
- prev: "index_htm_files/19016.png",
- prevdark: "index_htm_files/19017.png",
+ first: "index_htm_files/18928.png",
+ firstdark: "index_htm_files/18929.png",
+ last: "index_htm_files/18930.png",
+ lastdark: "index_htm_files/18931.png",
+ next: "index_htm_files/18932.png",
+ nextdark: "index_htm_files/18933.png",
+ prev: "index_htm_files/18934.png",
+ prevdark: "index_htm_files/18935.png",
  rows: "4",
  theme: "1"
 }
