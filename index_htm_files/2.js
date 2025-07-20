f6198c63a46ab17d33d64fa35c49f813e2a0bb32
Gerhard <diekettls@t-online.de>
2025-07-20 14:07:45 +0200

    Update website

--- index_htm_files/2.js
+++ index_htm_files/2.js
@@ -1,14 +1,14 @@
 window.local_table1_44_data = [
 {
  csv: "Herren:\u000A\u000A1. Mario Vogl\u000A2. Stefan Salzberger\u000A3. Stefan Weinmann",
- first: "index_htm_files/18928.png",
- firstdark: "index_htm_files/18929.png",
- last: "index_htm_files/18930.png",
- lastdark: "index_htm_files/18931.png",
- next: "index_htm_files/18932.png",
- nextdark: "index_htm_files/18933.png",
- prev: "index_htm_files/18934.png",
- prevdark: "index_htm_files/18935.png",
+ first: "index_htm_files/19021.png",
+ firstdark: "index_htm_files/19022.png",
+ last: "index_htm_files/19023.png",
+ lastdark: "index_htm_files/19024.png",
+ next: "index_htm_files/19025.png",
+ nextdark: "index_htm_files/19026.png",
+ prev: "index_htm_files/19027.png",
+ prevdark: "index_htm_files/19028.png",
  rows: "4",
  theme: "1"
 }
