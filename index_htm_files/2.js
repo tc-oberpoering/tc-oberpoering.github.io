b954b79fe074e2f0726928cbf1cc6cbe00b18239
Gerhard Kettl <diekettls@t-online.de>
2025-10-15 21:45:56 +0200

    Update website

--- index_htm_files/2.js
+++ index_htm_files/2.js
@@ -1,14 +1,14 @@
 window.local_table1_44_data = [
 {
  csv: "Herren:\u000A\u000A1. Mario Vogl\u000A2. Stefan Salzberger\u000A3. Stefan Weinmann",
- first: "index_htm_files/19119.png",
- firstdark: "index_htm_files/19120.png",
- last: "index_htm_files/19121.png",
- lastdark: "index_htm_files/19122.png",
- next: "index_htm_files/19123.png",
- nextdark: "index_htm_files/19124.png",
- prev: "index_htm_files/19125.png",
- prevdark: "index_htm_files/19126.png",
+ first: "index_htm_files/19203.png",
+ firstdark: "index_htm_files/19204.png",
+ last: "index_htm_files/19205.png",
+ lastdark: "index_htm_files/19206.png",
+ next: "index_htm_files/19207.png",
+ nextdark: "index_htm_files/19208.png",
+ prev: "index_htm_files/19209.png",
+ prevdark: "index_htm_files/19210.png",
  rows: "4",
  theme: "1"
 }
--- index_htm_files/3.js
+++ index_htm_files/3.js
@@ -1,14 +1,14 @@
 window.local_table1_45_data = [
 {
  csv: "Damen:\u000A\u000A1. Sabine Bauriedl\u000A2. Emma F\u00FChrmann\u000A3. Helena Freundorfer",
- first: "index_htm_files/19119.png",
- firstdark: "index_htm_files/19120.png",
- last: "index_htm_files/19121.png",
- lastdark: "index_htm_files/19122.png",
- next: "index_htm_files/19123.png",
- nextdark: "index_htm_files/19124.png",
- prev: "index_htm_files/19125.png",
- prevdark: "index_htm_files/19126.png",
+ first: "index_htm_files/19203.png",
+ firstdark: "index_htm_files/19204.png",
+ last: "index_htm_files/19205.png",
+ lastdark: "index_htm_files/19206.png",
+ next: "index_htm_files/19207.png",
+ nextdark: "index_htm_files/19208.png",
+ prev: "index_htm_files/19209.png",
+ prevdark: "index_htm_files/19210.png",
  rows: "4",
  theme: "1"
 }
