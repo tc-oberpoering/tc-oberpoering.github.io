d5e7426b89c3cea0dfcd37c46f02b744412c8e18
Gerhard Kettl <diekettls@t-online.de>
2025-11-28 11:20:35 +0100

    Update website

--- index_htm_files/3.js
+++ index_htm_files/3.js
@@ -1,14 +1,14 @@
 window.local_table1_46_data = [
 {
  csv: "Funktion, Name:\u000A,\u000AErster Vorstand, Tobias F\u00FChrmann\u000AZweiter Vorstand, Gerhard Kettl\u000AKassenwart, Franz Salzberger\u000ASchriftf\u00FChrer, Gerhard Kettl\u000APlatzwart 1, Thomas Ebner\u000APlatzwart 2, Edmund G\u00F6tzensberger\u000ASportwart, Stefan Salzberger\u000AJugendleitung, Stefan Weinmann\u000ABeisitzer 1, Sabine Bauriedl\u000ABeisitzer 2, Simone Gabler\u000ABeisitzer 3, Rudolf Freundorfer\u000ABeisitzer 4, Tobias Steinherr\u000ABeisitzer 5, Christian Weinmann\u000ABeisitzer 6, Sven Huber\u000A\u000A\u000AEhrenvorstand TC Oberp\u00F6ring, Franz Bauriedl\u000AEhrenmitglied TC Oberp\u00F6ring, Erwin Heigl",
- first: "index_htm_files/19332.png",
- firstdark: "index_htm_files/19333.png",
- last: "index_htm_files/19334.png",
- lastdark: "index_htm_files/19335.png",
- next: "index_htm_files/19336.png",
- nextdark: "index_htm_files/19337.png",
- prev: "index_htm_files/19338.png",
- prevdark: "index_htm_files/19339.png",
+ first: "index_htm_files/19203.png",
+ firstdark: "index_htm_files/19204.png",
+ last: "index_htm_files/19205.png",
+ lastdark: "index_htm_files/19206.png",
+ next: "index_htm_files/19207.png",
+ nextdark: "index_htm_files/19208.png",
+ prev: "index_htm_files/19209.png",
+ prevdark: "index_htm_files/19210.png",
  rows: "20",
  theme: "1"
 }
